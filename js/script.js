8e758202970fcdcd0fa6a6a1286dc1035960841d
Sara <saracorsob@gmail.com>
2020-05-19 16:03:33 +0200

    Richiesta nome e cognome + debug

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-document.getElementById('password').innerHTML = 'Bye';
+//Infos Utente
+var nomeUtente = prompt('Come ti chiami?');
+console.log(nomeUtente);
+
+var cognomeUtente = prompt('Qual è il tuo cognome?');
+console.log(cognomeUtente);
