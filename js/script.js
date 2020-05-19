f1b3f45dc33eb56847900f4e14178cf3a057a3b9
Sara <saracorsob@gmail.com>
2020-05-19 16:09:22 +0200

    Colore preferito e password + debug

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-//Infos Utente
+// Infos Utente
 var nomeUtente = prompt('Come ti chiami?');
 console.log(nomeUtente);
 
 var cognomeUtente = prompt('Qual è il tuo cognome?');
 console.log(cognomeUtente);
+
+var coloreUtente = prompt('Qual è il tuo colore preferito?');
+console.log(coloreUtente);
+
+// Generatore Password
+var passwordUtente = nomeUtente + cognomeUtente + coloreUtente + 19;
+console.log(passwordUtente);
+
+// Visualizzazione Password sulla pagina
+document.getElementById('password').innerHTML = 'La tua password è ' + passwordUtente;
